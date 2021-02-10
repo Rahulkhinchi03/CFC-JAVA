148044446d9f694edd5e60aac0a68bab0d5cfa39
Rahul <rahulkhinchirk7@gmail.com>
2021-02-10 20:05:12 +0530

    Done

--- src/Array/MaxArrayValue.java
+++ src/Array/MaxArrayValue.java
@@ -1,20 +1,16 @@
 package Array;
 
-import java.util.Scanner;
-
 public class MaxArrayValue {
     public static void main(String[] args) {
 
+        int[] value = {44,33,77,100,45,22,1,7};
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0;i< value.length-1;i++) {
+            if (value[i] > max) {
+                max = value[i];
+            }
+        }
+        System.out.println("Maximum value is " + max);
     }
-
-    Scanner sc = new Scanner(System.in);
-
-    public static int[] MaxArray(int[] array) {
-        
-
-
-return array;
-    }
-
-
 }
