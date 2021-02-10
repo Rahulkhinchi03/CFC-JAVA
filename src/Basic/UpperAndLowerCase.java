31f515c25e22f37ea6fe469b8b86a1e6e255d3b5
Rahul <rahulkhinchirk7@gmail.com>
2021-02-11 01:21:00 +0530

    Done

---
+++ src/Basic/UpperAndLowerCase.java
@@ -0,0 +1,21 @@
+package Basic;
+
+public class UpperAndLowerCase {
+    public static void main(String[] args) {
+         Character('r');
+
+    }
+
+    public static void Character(char Case) {
+
+        if(Case >= 'A' && Case <= 'Z') {
+            System.out.println("It is Uppercase");
+        }
+        if(Case >= 'a' && Case <= 'z') {
+            System.out.println("It is Lowercase");
+        }
+
+
+
+    }
+}
